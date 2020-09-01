2042779f1c973b4d912e33bf5e86b677b179d3c7
liuwenquan <liuwenquan@zving.com>
2020-09-01 17:20:30 +0800

    first commit

---
+++ profile.ps1
@@ -0,0 +1,14 @@
+$today = Get-Date
+[string]$formatTime = $today.ToString('yyyy-MM-dd HH:mm:ss')
+[int]$hour = $today.Hour
+[string]$timeSlot
+if ($hour -lt 12) {
+  $timeSlot = '上午'
+}
+if ($hour -lt 18 && $hour -ge 12) {
+  $timeSlot = '下午'
+}
+if ($hour -ge 18) {
+  $timeSlot = '晚上'
+}
+Write-Host $timeSlot'好，先生,现在是北京时间'$formatTime
